db324ff6ac50f25a8d4149773fd63d493c1b0725
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2025-05-23 18:54:39 +0300

    Added the widget from meteoagent.com

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('space weather');
+// // Посилання на дані
+// const serverOne = 'https://kp.gfz.de/app/json/?start=2022-01-30T00:00:00Z&end=2022-02-02T23:59:59Z&index=C9&status=def';
+
+// // Асинхронна функція
+// async function loadData(data) {
+//     try {
+//         const response = await fetch(data);
+//         const responseResult = await response.json();
+        
+//         console.log(responseResult);
+        
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+
+// loadData(serverOne);
