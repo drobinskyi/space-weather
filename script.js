beaa668396f383513270d8ecd724a59fa8d1a911
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2025-05-24 12:59:26 +0300

    Created new api from NOAA

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// // Посилання на дані
-// const serverOne = 'https://kp.gfz.de/app/json/?start=2022-01-30T00:00:00Z&end=2022-02-02T23:59:59Z&index=C9&status=def';
+// Посилання на дані
+const dataNOAA = 'https://services.swpc.noaa.gov/products/noaa-planetary-k-index-forecast.json';
 
-// // Асинхронна функція
-// async function loadData(data) {
-//     try {
-//         const response = await fetch(data);
-//         const responseResult = await response.json();
+// Асинхронна функція
+async function loadData(data) {
+    try {
+        const response = await fetch(data);
+        const responseResult = await response.json();
         
-//         console.log(responseResult);
+        console.log(responseResult);
         
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-// loadData(serverOne);
+loadData(dataNOAA);
